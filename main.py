c576256eaccb38175a7c9ae2dc2a1001d4821e59
Afshin Oroojlooy <oroojlooy@gmail.com>
2020-07-01 19:25:10 -0400

    refined the imports

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import sys
 from clBeergame import *
 from utilities import *
 import numpy as np 
@@ -75,16 +74,6 @@ def main(config):
 	demandTs = np.load(direc)	
 	print("loaded test set=", direc)
 
-	if config.maxEpisodesTrain == -1:
-	# demandTs = np.array([[2,6,6,1,3,1,0,8,0,6,6,3,0,8,0,1,2,5,8,2,6,2,4,4,8,1,0,6,4,2,2,7,7,4,7,7,2,2,5,0,3,4,6,1,1,5,2,0,0,7,3,1,0,6,3,8,2,4,1,8,6,2,3,7,5,4,7,8,8,3,5,5,5,0,1,7,4,5,3,1,3,3,1,0,0,1,4,5,5,0,4,1,3,8,5,1,4,0,4,2]])
-	# demandTs = np.array([[4,4,4,4,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]])
-	# demandTs = np.array([[9,11,9,9,8,7,12,11,12,13,6,11,8,11,11,12,12,9,10,10,10,8,13,9,8,12,12,8,8,9,10,8,7,12,10,5,7,11,13,13,6,8,13,11,9,10,8,10,8,9,13,11,9,6,10,9,11,9,7,10,13,9,8,12,9,9,11,10,5,9,7,12,14,10,11,10,10,12,8,10,8,11,9,7,11,10,13,10,13,13,9,9,14,11,12,7,12,11,9,12]])
-		# demandTs = np.load('data/demandTs003.npy')
-		demandTs = np.load('data/demandTs110.npy')
-		# demandTs = np.concatenate((4*np.ones((10,4)) ,8*np.ones((10,config.Ttest+10))), axis=1).astype(int)
-		# config.f = [config.f1, config.f2, config.f3, config.f4] 
-		print("loaded auxiliary data")
-		# config.agentTypes = ["optm", "optm","optm","optm"]
 
 	# initilize an instance of Beergame
 	beerGame = clBeerGame(config)
@@ -111,5 +100,4 @@ if __name__ == '__main__':
 	config, unparsed = get_config()
 
 	# run main
-	#tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
 	main(config)
